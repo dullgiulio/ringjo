25d8e629e26fc9172f82339344b413ab7727ab6e
Giulio Iotti <dullgiulio@gmail.com>
2017-12-11 08:44:24 +0100

    ring: use vertx futures instead of Java native

--- src/main/java/ring/Executor.java
+++ src/main/java/ring/Executor.java
@@ -1,14 +1,16 @@
 package ring;
 
-import ring.futures.ReaderFuture;
+import io.vertx.core.Future;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Executor implements Runnable {
 	private Ring ring;
-	private BlockingQueue<Callable<CompletableFuture<Boolean>>> queue;
+	private BlockingQueue<Callable<Future<Boolean>>> queue;
 	private Logger logger = Logger.getLogger("RING");
 
 	public Executor(int ringSize, int queueSize) {
@@ -17,17 +19,17 @@ public class Executor implements Runnable {
 	}
 
 	public Future<Reader> read(Reader r) throws InterruptedException {
-		ReaderFuture readerFuture = new ReaderFuture();
+		Future<Reader> readerFuture = Future.future();
 		submit(() -> {
 			ring.read(r);
-			readerFuture.put(r);
+			readerFuture.complete(r);
 			return null;
 		});
 		return readerFuture;
 	}
 
 	public Future<Boolean> write(Message msg) throws InterruptedException {
-		CompletableFuture<Boolean> future = new CompletableFuture<>();
+		Future<Boolean> future = Future.future();
 		submit(() -> {
 			ring.write(msg);
 			future.complete(true);
@@ -37,7 +39,7 @@ public class Executor implements Runnable {
 	}
 
 	public Future<Boolean> writeBatch(Iterable<Message> msgs) throws InterruptedException {
-		CompletableFuture<Boolean> future = new CompletableFuture<>();
+		Future<Boolean> future = Future.future();
 		submit(() -> {
 			for (Message msg : msgs) {
 				ring.write(msg);
@@ -48,22 +50,22 @@ public class Executor implements Runnable {
 		return future;
 	}
 
-	public CompletableFuture<Boolean> stop() throws InterruptedException {
-		CompletableFuture<Boolean> stopped = new CompletableFuture<>();
+	public Future<Boolean> stop() throws InterruptedException {
+		Future<Boolean> stopped = Future.future();
 		submit(() -> {
 			return stopped; // Stop execution and signal back.
 		});
 		return stopped;
 	}
 
-	private void submit(Callable<CompletableFuture<Boolean>> c) throws InterruptedException {
+	private void submit(Callable<Future<Boolean>> c) throws InterruptedException {
 		queue.put(c);
 	}
 
 	@Override
 	public void run() {
 		while (true) {
-			Callable<CompletableFuture<Boolean>> c;
+			Callable<Future<Boolean>> c;
 			try {
 				c = queue.take();
 			} catch (InterruptedException e) {
@@ -71,7 +73,7 @@ public class Executor implements Runnable {
 				return;
 			}
 			try {
-				CompletableFuture<Boolean> done = c.call();
+				Future<Boolean> done = c.call();
 				if (done != null) {
 					logger.info("Loop shutting down, exiting...");
 					// ... could do any cleanup here.
--- src/test/java/ring/ExecutorTest.java
+++ src/test/java/ring/ExecutorTest.java
@@ -1,36 +1,63 @@
 package ring;
 
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 import static org.junit.Assert.assertEquals;
 
 public class ExecutorTest {
-	@Test
-	public void executorOnSeparateThread() throws InterruptedException, ExecutionException {
-		Executor executor = new Executor(10, 1024);
-		Thread t = new Thread(executor);
-		t.start();
-		Reader r = new Reader("test reader", 2);
-		executor.write(new Message("test 1"));
-		executor.write(new Message("test 2"));
-		Future<Reader> fr = executor.read(r);
-		r = fr.get();
+	Executor executor;
+	Thread executorThread;
+
+	@Before
+	public void setUp() {
+		executor = new Executor(10, 1024);
+		executorThread = new Thread(executor);
+	}
+
+	private void checkFirstRead(AsyncResult<Reader> ar) {
+		assertEquals(true, ar.succeeded());
+		Reader r = ar.result();
 		List<Message> buffer = r.getBuffer();
 		assertEquals(2, buffer.size());
 		Message msg = buffer.get(0);
 		assertEquals("test 1", new String(msg.getContent()));
 		msg = buffer.get(1);
 		assertEquals("test 2", new String(msg.getContent()));
-		fr = executor.read(r);
-		r = fr.get();
-		buffer = r.getBuffer();
+	}
+
+	private void checkSecondRead(AsyncResult<Reader> ar) {
+		assertEquals(true, ar.succeeded());
+		Reader r = ar.result();
+		List<Message> buffer = r.getBuffer();
 		assertEquals(0, buffer.size());
+	}
+
+	private void checkThreadJoinable(AsyncResult<Boolean> ar) {
+		assertEquals(true, ar.succeeded());
+		try {
+			executorThread.join();
+		} catch(InterruptedException e) {
+			System.out.printf("Interrupted\n");
+		}
+	}
+
+	@Test
+	public void executorOnSeparateThread() throws InterruptedException {
+		executorThread.start();
+		Reader r = new Reader("test reader", 2);
+		executor.write(new Message("test 1"));
+		executor.write(new Message("test 2"));
+		Future<Reader> fr = executor.read(r);
+		fr.setHandler(this::checkFirstRead);
+		fr = executor.read(r);
+		fr.setHandler(this::checkSecondRead);
 		Future<Boolean> fb = executor.stop();
-		fb.get();
-		t.join();
+		fb.setHandler(this::checkThreadJoinable);
 	}
 }
